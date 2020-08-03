fda7cbaf05fab0dc9e45b1655f8625f9c2b9de6c
i1620 <68018020+i1620@users.noreply.github.com>
2020-08-03 16:54:04 -0500

    added homework

---
+++ PyBank/main.py
@@ -0,0 +1,79 @@
+import csv
+import os
+
+budgetFilePath = os.path.join("Resources/" + "budget_data.csv")
+
+total = 0
+profit = 0
+loss = 0
+total_amount = 0
+month= []
+revenue = []
+revenue_change = []
+avg_change = []
+
+#read csv
+with open(budgetFilePath ) as csvFile:
+	csvReader = csv.reader(csvFile, delimiter=",")
+
+	csvRow = next(csvReader)
+
+#find number of months
+	month_total = sum(1 for row in csvReader)
+
+	print("Financial Analysis")
+	print("-----------------------------")
+	print(f"Total Months: {month_total}")
+
+	csvFile.seek(0)
+	next(csvReader)
+
+#find the total amount
+	for row in csvReader:
+		month.append(row[0])
+		revenue.append(row[1])
+		total = int(row[1])
+		if total > 0:
+			profit = total + profit
+		elif total < 0:
+			loss = total + loss
+	total_amount = profit + loss
+
+	print(f"Total: ${total_amount}")
+
+
+#find the average change
+	i = 0
+	for i in range(len(revenue)-1):
+		revenue_loss = int(revenue[i +1])- int(revenue[i])
+		revenue_change.append(revenue_loss)
+	Total = sum(revenue_change)
+	avg_change = Total / len(revenue_change)
+	avg = str(round(avg_change,2))
+
+	print(f"Average Change : ${avg}")
+
+#find greatest increase in profits
+	inc_change = max(revenue_change)
+	inc = revenue_change.index(inc_change)
+	month_inc = month[inc+1]
+
+	print(f"Greatest Increase in Profits : {month_inc} (${inc_change})")
+
+#find greatest decrease in profits:
+	dec_change = min(revenue_change)
+	dec = revenue_change.index(dec_change)
+	month_dec = month[dec+1]
+
+	print(f"Greatest Decrease in Profits : {month_dec} (${dec_change})")
+
+outputPath = os.path.join("analysis/" + "PyBank.py")
+with open(outputPath, 'w', newline='') as text_file:
+
+	print("Financial Analysis", file=text_file)
+	print("-----------------------------", file=text_file)
+	print(f"Total Months: {month_total}", file=text_file)
+	print(f"Total: ${total_amount}", file=text_file)
+	print(f"Average Change : ${avg}", file=text_file)
+	print(f"Greatest Increase in Profits : {month_inc} (${inc_change})", file=text_file)
+	print(f"Greatest Decrease in Profits : {month_dec} (${dec_change})", file=text_file)
---
+++ PyPoll/main.py
@@ -0,0 +1,89 @@
+import csv
+import os
+
+#possible variables
+total_votes = []
+candidates = []
+candidate_1 = []
+candidate_2 = []
+candidate_3 = []
+candidate_4 = []
+
+electionFilePath = os.path.join("Resources/" + "election_data.csv")
+
+#read csv
+with open(electionFilePath ) as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=",")
+
+    csvRow = next(csvReader)
+
+#find total number of votes cast
+    for row in csvReader:
+        total_votes.append(row[0])
+        candidates.append(row[2])
+
+    t_votes = len(total_votes)
+
+    print("Election Results")
+    print("-----------------------------")
+    print(f"Total Votes: {t_votes}")
+    print("-----------------------------")
+
+#find number of votes for each candidate
+    for row[2] in candidates:
+        if row[2] == "Khan":
+            candidate_1.append(candidates)
+            c1_votes = len(candidate_1)
+        elif row[2] == "Correy":
+            candidate_2.append(candidates)
+            c2_votes = len(candidate_2) 
+        elif row[2] == "Li":
+            candidate_3.append(candidates)
+            c3_votes = len(candidate_3)
+        else:
+            candidate_4.append(candidates)
+            c4_votes = len(candidate_4)
+    
+#find percentage of votes for each candidate
+    c1_percent = round(((c1_votes/t_votes)*100), 2)
+    c1_per = format(c1_percent, '.3f')
+    c2_percent = round(((c2_votes/t_votes)*100), 2)
+    c2_per = format(c2_percent, '.3f')
+    c3_percent = round(((c3_votes/t_votes)*100), 2)
+    c3_per = format(c3_percent, '.3f')
+    c4_percent = round(((c4_votes/t_votes)*100), 2)
+    c4_per = format(c4_percent, '.3f')
+
+    print(f"Khan: {c1_per}% ({c1_votes})")
+    print(f"Correy: {c2_per}% ({c2_votes})")
+    print(f"Li: {c3_per}% ({c3_votes})")
+    print(f"O'Toole: {c4_per}% ({c4_votes})")
+    print("-----------------------------")
+
+#find winner
+    if c1_per > max(c2_per, c3_per, c4_per):
+        winner = "Khan"
+    elif c2_per > max(c1_per, c3_per, c4_per):
+        winner = "Correy"
+    elif c3_per > max(c1_per, c2_per, c4_per):
+        winner = "Li"
+    else:
+        winner = "O'Toole"
+    
+    print(f"Winner: {winner}")
+    print("-----------------------------")
+
+outputPath = os.path.join("analysis/" + "PyPoll.py")
+with open(outputPath, 'w', newline='') as text_file:
+
+    print("Election Results", file=text_file)
+    print("-----------------------------", file=text_file)
+    print(f"Total Votes: {t_votes}", file=text_file)
+    print("-----------------------------", file=text_file)
+    print(f"Khan: {c1_per}% ({c1_votes})", file=text_file)
+    print(f"Correy: {c2_per}% ({c2_votes})", file=text_file)
+    print(f"Li: {c3_per}% ({c3_votes})", file=text_file)
+    print(f"O'Toole: {c4_per}% ({c4_votes})", file=text_file)
+    print("-----------------------------", file=text_file)
+    print(f"Winner: {winner}", file=text_file)
+    print("-----------------------------", file=text_file)
